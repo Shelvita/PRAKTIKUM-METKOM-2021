1bd2f009b80cb36b15986df05aa9c93db2f150b4
Shelvita Nindya Paramitha <82561357+Shelvita@users.noreply.github.com>
2021-04-21 22:24:33 +0700

    Add files via upload

---
+++ halo.py
@@ -0,0 +1,81 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "better-skating",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Program halo.py\n",
+      "by shelvitaparamitha9999@mail.ugm.ac.id\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"Program halo.py\")\n",
+    "print(\"by shelvitaparamitha9999@mail.ugm.ac.id\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "id": "killing-advancement",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "#|==========================================|\n",
+      "#|                                          |\n",
+      "#|             Program halo.py              |\n",
+      "#| by shelvitaparamitha9999@mail.ugm.ac.id  |\n",
+      "#|                                          |\n",
+      "#|==========================================|\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"#|==========================================|\")\n",
+    "print(\"#|                                          |\")\n",
+    "print(\"#|             Program halo.py              |\")\n",
+    "print(\"#| by shelvitaparamitha9999@mail.ugm.ac.id  |\")\n",
+    "print(\"#|                                          |\")\n",
+    "print(\"#|==========================================|\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "bizarre-commons",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
